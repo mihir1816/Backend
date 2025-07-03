3cc0b5731b3c3155e1ef4f0833ba4ac61bcc2042
Mihir Jan <143832997+mihir1816@users.noreply.github.com>
2025-07-03 22:43:36 +0530

    cors origin change

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 const app = express() ; 
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN , 
+    origin :"https://yt-frontend-3wa2.onrender.com", 
     credentials : true 
 }))
 
